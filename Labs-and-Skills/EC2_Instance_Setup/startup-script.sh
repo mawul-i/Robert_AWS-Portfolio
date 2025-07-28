11d1141609781eee0b6b9f99467a9401bccde852
mawul-i <123276915+mawul-i@users.noreply.github.com>
2025-07-28 00:40:07 +0000

    Update startup-script.sh

--- Labs-and-Skills/EC2_Instance_Setup/startup-script.sh
+++ Labs-and-Skills/EC2_Instance_Setup/startup-script.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# EC2 instance initialization script for AWS re/Start lab
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Robert's EC2 Lab - Powered by AWS re/Start</h1>" > /var/www/html/index.html
 
